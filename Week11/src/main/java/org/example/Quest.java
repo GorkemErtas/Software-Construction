beefe6c6fe8ba909b1730bf2085bc6d1791e2427
Görkem Ertaş <162305182+GorkemErtas@users.noreply.github.com>
2024-05-20 17:17:34 +0300

    Add files via upload

---
+++ Week11/src/main/java/org/example/Quest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+/**
+ * Represents a quest in the lord of the rings world
+ */
+public class Quest {
+    /**
+     * Completes a quest based on points
+     *
+     * @param points       The points earned in the quest
+     * @param isRingBearer True if the player is the ring bearer, false otherwise.
+     * @return True if the quest is completed, false otherwise.
+     */
+    public boolean completeQuest(int points, boolean isRingBearer) {
+        boolean isQualified = points > 0 && isRingBearer || points < -100 || points == 42;
+
+        if (points > 50) {
+            if (points < 100) {
+                if (points % 2 == 0) {
+                    if (isQualified) {
+                        System.out.println("Qualified");
+                    }
+                }
+            }
+        }
+
+        return points >= 100;
+    }
+
+    /**
+     * Attempts to defend Helm's Deep if the player has armies and the enemy forces are manageable.
+     *
+     * @param hasArmies   True if the player has armies, false otherwise.
+     * @param enemyForces The number of enemy forces attacking Helm's Deep.
+     * @return True if Helm's Deep is successfully defended, false otherwise.
+     */
+    public boolean protectCastle(boolean hasArmies, int enemyForces) {
+        boolean canDefend = hasArmies && enemyForces <= 1200;
+
+        if (canDefend) {
+            System.out.println("Castle Defended!");
+        }
+
+        return canDefend;
+    }
+}
